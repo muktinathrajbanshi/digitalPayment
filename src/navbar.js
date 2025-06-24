e3790cb44d0d4a79e93a91533107c38049a13c9b
unknown <muktinathrajbanshi786@gmail.com>
2025-06-24 22:55:15 +0545

    navbar section completed along with mobile responsive

--- src/navbar.js
+++ src/navbar.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Navbar = () => {
+
+    const [show, setShow] = useState(false);
+
   return (
     <>
         <section className="navbar_bg">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
             <a className="navbar-brand" href="#">MuktiPay</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+            onClick={() => setShow(!show)}>
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className={`collapse navbar-collapse ${show ? "show" : "" }`} >
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
